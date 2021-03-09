f443225df324edf961f0d06298ea2556b1bff936
Realta <67647711+RealtaRose@users.noreply.github.com>
2021-03-08 20:24:51 -0600

    Initialized branch (#2)
    Co-authored-by: Caira Flynn <caira.roseflynn.com>

---
+++ FishFood/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace FishFood.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
